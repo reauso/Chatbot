a9ade5ad29190a30f948e6adf4edf4398482831f
Rene <r.ebertowski@gmx.de>
2022-12-13 11:26:23 +0100

    Changed Tyrion Lines extraction from Transcripts.

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -5,22 +5,29 @@ import re
 import util
 
 if __name__ == "__main__":
-    os.chdir(path.dirname(path.dirname(path.realpath(__file__))))
-    transcripts_path = path.join(os.getcwd(), 'Data', 'Transcripts')
+    import os
+    import os.path as path
+    import re
 
-    # get all episode file paths
-    episode_file_paths = util.files_in_directory(transcripts_path, '**/episode *.txt', recursive=True)
+    import util
 
-    # define necessary values
-    question_answer_pattern = r'(?i): ([^\n]*\n*)\btyrion\b.*:(.*)'
-    question_answer_pairs = []
+    if __name__ == "__main__":
+        os.chdir(path.dirname(path.dirname(path.realpath(__file__))))
+        transcripts_path = path.join(os.getcwd(), 'Data', 'Transcripts')
 
-    # read and process episodes
-    for episode_file in episode_file_paths:
-        episode = open(episode_file, 'r', encoding='utf8')
-        episode_content = episode.read()
+        # get all episode file paths
+        episode_file_paths = util.files_in_directory(transcripts_path, '**/episode *.txt', recursive=True)
 
-        question_answer_pairs.extend(re.findall(question_answer_pattern, episode_content))
+        # define necessary values
+        question_answer_pattern = r'(?i): ([^\n]*\n*)\btyrion\b.*:(.*)'
+        question_answer_pairs = []
 
-    print(question_answer_pairs[:10])
-    print(len(question_answer_pairs))
+        # read and process episodes
+        for episode_file in episode_file_paths:
+            episode = open(episode_file, 'r', encoding='utf8')
+            episode_content = episode.read()
+
+            question_answer_pairs.extend(re.findall(question_answer_pattern, episode_content))
+
+        print(question_answer_pairs[:10])
+        print(len(question_answer_pairs))
